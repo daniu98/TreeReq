00bf78bb876740ae5fca83f9872041d2e810fb76
maxwbregler <max.wes.bregler@gmail.com>
2026-04-24 18:24:33 -0700

    removed username from auth, refactored to be consistent with frontend

--- backend/auth.py
+++ backend/auth.py
@@ -4,42 +4,29 @@ import bcrypt
 # export MONGO_URI DB_NAME
 client = pymongo.MongoClient(os.environ["MONGO_URI"])
 db = client.get_database(os.environ["DB_NAME"])
-def createUser(username, email, password):
-    # 0 - SUCCESS, 1 - USERNAME OR EMAIL TAKEN, 2 - INVALID CREDENTIALS 3 - OTHER ERROR
-    if(((".com" in email) == False) or (" " in username) or (" " in email) or (" " in password)):
+def signup(email, password):
+    # 0 - SUCCESS, 1 - EMAIL TAKEN, 2 - INVALID CREDENTIALS 3 - OTHER ERROR
+    if((("@" in email) == False) or (("." in email) == false) or (" " in email) or (" " in password)):
         return 2
-    usersWithName = db.users.find_one({"username": username})
-    if usersWithName == None:
-        usersWithEmail = db.users.find_one({"email": email})
-        if usersWithEmail == None:
-            if(db.users.find_one({"email": username}) == None and db.users.find_one({"username": email}) == None):
-                passwordBytes = password.encode('utf-8')
-                bCryptSalt = bcrypt.gensalt()
-                passwordHash = bcrypt.hashpw(passwordBytes, bCryptSalt)
-                db.users.insert_one({"username": username, "email": email, "password": passwordHash})
-                if db.users.find_one({"username": username}) == None:
-                    return 3
-                else:
-                    return 0
-            else:
-                return 1
-        else:
-            return 1
-    else:
-        return 1
-def signinUser(usernameOrEmail, password):
-    # 0 - SUCCESS, 1 = INCORRECT USERNAME/EMAIL OR PASSWORD
-    usersWithName = db.users.find_one({"username": usernameOrEmail})
-    if usersWithName:
+    usersWithEmail = db.users.find_one({"email": email})
+    if usersWithEmail == None:
         passwordBytes = password.encode('utf-8')
-        realPasswordHash = db.users.find_one({"username": usernameOrEmail}).get("password")
-        if(bcrypt.checkpw(passwordBytes, realPasswordHash)):
-           return 0
-        print(db.users.find_one({"username": usernameOrEmail}).get("password"))
-    usersWithEmail = db.users.find_one({"email": usernameOrEmail})
+        bCryptSalt = bcrypt.gensalt()
+        passwordHash = bcrypt.hashpw(passwordBytes, bCryptSalt)
+        db.users.insert_one({"email": email, "password": passwordHash})
+        if db.users.find_one({"email": email}) == None:
+            return 3
+        else:
+            return 0
+    else:
+        print(usersWithEmail.get("password"))
+        return 1
+def login(email, password):
+    # 0 - SUCCESS, 1 = INCORRECT EMAIL OR PASSWORD
+    usersWithEmail = db.users.find_one({"email": email})
     if usersWithEmail:
         passwordBytes = password.encode('utf-8')
-        realPasswordHash = db.users.find_one({"email": usernameOrEmail}).get("password")
+        realPasswordHash = db.users.find_one({"email": email}).get("password")
         if(bcrypt.checkpw(passwordBytes, realPasswordHash)):
             return 0
     return 1;
