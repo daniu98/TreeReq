f6771e13bc1c9fa8cd283f1d48ec37063d04902c
maxwbregler <max.wes.bregler@gmail.com>
2026-04-26 16:13:33 -0700

    making auth.py compatible with authApi.js

--- backend/auth.py
+++ backend/auth.py
@@ -1,15 +1,20 @@
 import pymongo
 import os
 import bcrypt
+import json
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+origins = ["http://localhost:5173", "http://localhost:3000"]
+app.add_middleware(CORSMiddleware, allow_origins = origins, allow_credentials = True, allow_methods = ["*"], allow_headers = ["*"])
 # export MONGO_URI DB_NAME
 client = pymongo.MongoClient(os.environ["MONGO_URI"])
 db = client.get_database(os.environ["DB_NAME"])
-@app.get("/api/signup")
-def signup(email, password):
-    # 0 - SUCCESS, 1 - EMAIL TAKEN, 2 - INVALID CREDENTIALS 3 - OTHER ERROR
-    if((("@" in email) == False) or (("." in email) == false) or (" " in email) or (" " in password)):
+@app.post("/api/auth/signup")
+def signup(inputJson):
+    email = json.loads(inputJson)["email"]
+    password = json.loads(inputJson)["password"]
+    if((("@" in email) == False) or (("." in email) == False) or (" " in email) or (" " in password)):
         return 2
     usersWithEmail = db.users.find_one({"email": email})
     if usersWithEmail == None:
@@ -17,20 +22,17 @@ def signup(email, password):
         bCryptSalt = bcrypt.gensalt()
         passwordHash = bcrypt.hashpw(passwordBytes, bCryptSalt)
         db.users.insert_one({"email": email, "password": passwordHash})
-        if db.users.find_one({"email": email}) == None:
-            return 3
-        else:
-            return 0
+        return {"message": "0 - SUCCESS"}
     else:
-        print(usersWithEmail.get("password"))
-        return 1
-@app.get("/api/login")
-def login(email, password):
-    # 0 - SUCCESS, 1 = INCORRECT EMAIL OR PASSWORD
+        return {"message": "1 - INVALID INPUTS/EMAIL TAKEN"}
+@app.post("/api/auth/login")
+def login(inputJson):
+    email = json.loads(inputJson)["email"]
+    password = json.loads(inputJson)["password"]
     usersWithEmail = db.users.find_one({"email": email})
     if usersWithEmail:
         passwordBytes = password.encode('utf-8')
-        realPasswordHash = db.users.find_one({"email": email}).get("password")
+        uswersWithEmail.get("password")
         if(bcrypt.checkpw(passwordBytes, realPasswordHash)):
-            return 0
-    return 1;
+            return {"message": "0 - SUCCESS"}
+    return {"message": "1 - INCORRECT EMAIL/PASSWORD"}
