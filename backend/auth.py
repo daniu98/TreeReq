1025d05aaa454a43fbbb1d6aee1ab5f18c1b7aa0
maxwbregler <max.wes.bregler@gmail.com>
2026-04-26 16:35:35 -0700

    finished auth endpoints

--- backend/auth.py
+++ backend/auth.py
@@ -2,6 +2,7 @@ import pymongo
 import os
 import bcrypt
 import json
+from pydantic import BaseModel
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
@@ -10,29 +11,32 @@ app.add_middleware(CORSMiddleware, allow_origins = origins, allow_credentials = 
 # export MONGO_URI DB_NAME
 client = pymongo.MongoClient(os.environ["MONGO_URI"])
 db = client.get_database(os.environ["DB_NAME"])
+class AuthData(BaseModel):
+    email: str
+    password: str
 @app.post("/api/auth/signup")
-def signup(inputJson):
-    email = json.loads(inputJson)["email"]
-    password = json.loads(inputJson)["password"]
+def signup(data: AuthData):
+    email = data.email
+    password = data.password
     if((("@" in email) == False) or (("." in email) == False) or (" " in email) or (" " in password)):
-        return 2
+        return {"message": "1 - INVALID INPUTS/EMAIL TAKEN"}
     usersWithEmail = db.users.find_one({"email": email})
     if usersWithEmail == None:
         passwordBytes = password.encode('utf-8')
         bCryptSalt = bcrypt.gensalt()
         passwordHash = bcrypt.hashpw(passwordBytes, bCryptSalt)
         db.users.insert_one({"email": email, "password": passwordHash})
-        return {"message": "0 - SUCCESS"}
+        return {"message": "Successfully signed up"}
     else:
-        return {"message": "1 - INVALID INPUTS/EMAIL TAKEN"}
+        return {"message": "Invalid inputs or Email taken"}
 @app.post("/api/auth/login")
-def login(inputJson):
-    email = json.loads(inputJson)["email"]
-    password = json.loads(inputJson)["password"]
+def login(data: AuthData):
+    email = data.email
+    password = data.password
     usersWithEmail = db.users.find_one({"email": email})
     if usersWithEmail:
         passwordBytes = password.encode('utf-8')
-        uswersWithEmail.get("password")
-        if(bcrypt.checkpw(passwordBytes, realPasswordHash)):
-            return {"message": "0 - SUCCESS"}
-    return {"message": "1 - INCORRECT EMAIL/PASSWORD"}
+        usersWithEmail.get("password")
+        if(bcrypt.checkpw(passwordBytes, usersWithEmail.get("password"))):
+            return {"message": "Successsfully signed in"}
+    return {"message": "Incorrect email or password"}
