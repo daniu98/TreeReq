a63bafd45db2d490c2850a6d10cc1ac1d1ecc7b4
maxwbregler <max.wes.bregler@gmail.com>
2026-05-21 16:06:46 -0700

    added backend router to check if user has already onboarded

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from config.database import connect_db, close_db
-from routers import auth_sso, courses, majors, onboarding, submit_onboarding_data
+from routers import auth_sso, courses, majors, onboarding, submit_onboarding_data, check_if_onboarded
 import pymongo
 import os
 
@@ -24,6 +24,7 @@ app.include_router(majors.router, prefix="/api")
 app.include_router(auth_sso.router, prefix="/api")
 app.include_router(onboarding.router, prefix="/api")
 app.include_router(submit_onboarding_data.router, prefix="/api")
+app.include_router(check_if_onboarded.router, prefix="/api")
 # export MONGO_URI DB_NAME
 client = pymongo.MongoClient(os.environ["MONGO_URI"])
 db = client.get_database(os.environ["DB_NAME"])
